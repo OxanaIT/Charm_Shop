48cec5d526d0aed4bdffeee66ac5f63c05b09c69
User <user@ITcourse.com>
2021-08-19 16:51:09 +0300

    Create landing page

---
+++ charm_shop/main_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('women/', views.women, name="women"),
+    path('men/', views.men, name="men"),
+    path('kids/', views.kids, name="kids"),
+    path('cart/', views.cart, name="cart"),
+    path('profile/', views.profile, name="profile")
+]
--- charm_shop/main_app/views.py
+++ charm_shop/main_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'main_app/home.html')
+
+
+def women(request):
+    return render(request, 'main_app/women.html')
+
+
+def men(request):
+    return render(request, 'main_app/men.html')
+
+
+def kids(request):
+    return render(request, 'main_app/kids.html')
+
+
+def cart(request):
+    return render(request, 'main_app/cart.html')
+
+
+def profile(request):
+    return render(request, 'main_app/profile.html')
+
+"""
+def new_search(request):
+    search = request.POST.get("search")
+    print(search)
+    return render(request, 'main_app/home.html')
+"""
