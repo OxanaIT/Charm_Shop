d80b1f7688cd0b68ee5ad16036b3df70e1984712
Laura Ozolina <laurai.ozolinai@gmail.com>
2021-08-21 16:53:36 +0300

    Create product list database and pictures

--- charm_shop/main_app/models.py
+++ charm_shop/main_app/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# creating Customer model with user that is one to one one user one cart one page. + import from
+# django auth.model - User. name, address + when created the account
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    joined_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
+
+
+# creating category model with title and slug as unique
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class SubCategory(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "SubCategories"
+
+
+# creating Product model with info about each product. slug like url unique.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.FloatField()
+    return_policy = models.CharField(max_length=300, null=True, blank=True)
+    description = models.TextField()
+    image = models.CharField(max_length=1000, null=False, default='Picture not available')
+
+    def __str__(self):
+        return self.title
+
+
+# creating cart model with connection with Customer model, + total items in cart + when added
+class Cart(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    total = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # returning id of cart
+    def __str__(self):
+        return "Cart: " + str(self.id)
+
+
+# create cartproduct - this is shown inside cart
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    subtotal = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "Cart: " + str(self.cart.id) + "CartProduct: " + str(self.id)
+
+
+ORDER_STATUS = {
+    ("Order Received", "Order Received"),
+    ("Order Processing", "Order Processing"),
+    ("On the way", "On the way"),
+    ("Order Completed", "Order Completed"),
+    ("Order Canceled", "Order Canceled"),
+}
+
+
+class Order(models.Model):
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
+    ordered_by = models.CharField(max_length=200)
+    shipping_address = models.CharField(max_length=200)
+    mobile = models.CharField(max_length=10)
+    email = models.EmailField(null=True, blank=True)
+    subtotal = models.PositiveIntegerField()
+    total = models.PositiveIntegerField()
+    order_status = models.CharField(max_length=50, choices=ORDER_STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Order: " + str(self.id)
--- charm_shop/main_app/views.py
+++ charm_shop/main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import TemplateView
 
 # Create your views here.
 def home(request):
@@ -23,6 +25,26 @@ def cart(request):
 
 def profile(request):
     return render(request, 'main_app/profile.html')
+
+
+def all_products(request):
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'main_app/all_products.html', context)
+
+
+class ProductDetailView(TemplateView):
+    template_name = "main_app/productdetail.html"
+
+    # dynamic content is not present, we have to send it from our backend
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_slug = self.kwargs['slug']
+        product = Product.objects.get(slug=url_slug)
+        context['product'] = product
+        return context
 
 """
 def new_search(request):
